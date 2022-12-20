5bcef9ed2d6dbc61216f47a5b961fe951b8a9e11
Maxim Yarmosh <maxinc636@gmail.com>
2022-12-20 21:07:33 +0300

    homework

---
+++ src/task2.java
@@ -0,0 +1,10 @@
+public class task2 {
+    public static void main(String...args) {
+        int sideA = 10;
+        int sideB = 22;
+        int perimeter = (sideA + sideB) * 2;
+        int square = sideA * sideB;
+            System.out.println("perimeter=" + perimeter + ";" + "Square=" + square);
+    }
+}
+
---
+++ src/task3.java
@@ -0,0 +1,12 @@
+public class task3 {
+    public static void main(String...args) {
+        double radius = 23;
+        double area = Math.PI * radius * radius;
+            System.out.println("Area=" + area);
+    }
+}
+
+
+
+
+
