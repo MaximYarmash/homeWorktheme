a75a145d08f3180b829138be980166a9195774fd
Maxim Yarmosh <maxinc636@gmail.com>
2022-12-21 19:39:51 +0300

    logic

--- src/task3.java
+++ src/task3.java
@@ -2,7 +2,8 @@ public class task3 {
     public static void main(String...args) {
         double radius = 23;
         double area = Math.PI * radius * radius;
-            System.out.println("Area=" + area);
+        double line = 2 * Math.PI * radius;
+            System.out.println("Area=" + area + ";" + "Line=" + line);
     }
 }
 
